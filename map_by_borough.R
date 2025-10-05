a57b4b68b9ae5f38acb58c011eee658735795e79
emma-w1 <emmawong565@gmail.com>
2025-10-05 14:23:53 -0400

    created function, finished all boroughs

--- map_by_borough.R
+++ map_by_borough.R
@@ -1,26 +1,25 @@
-library(sf)
-library(ggplot2)
-library(tmap)
-library(tmaptools)
-library(leaflet)
-library(dplyr)
-library(nycgeo)
-library(dplyr)
-
-
-options(scipen = 999)
-
-data <- read.csv("./pollen.csv", header = TRUE)
-pollen_data <- data[,c("Borough","Influx_trees","Influx_trees_alrg","Influx_plat","Influx_que","Influx_amb",
-                       "Influx_ulm","Influx_pop","Influx_carya","Influx_poa","Influx_acer","Influx_fagus",
-                       "Influx_bet","Influx_frax","Latitude","Longitude")]
-pollen_data <- st_as_sf(pollen_data, coords = c("Longitude", "Latitude"),  # specify coordinate columns
-                        crs = 4326) # assign a coordinate reference system (WGS84)
-
-pollen_map = nyc_boundaries(geography = "tract") #NYC MAP
-
-
 borough_map <- function(borough) {
+  library(sf)
+  library(ggplot2)
+  library(tmap)
+  library(tmaptools)
+  library(leaflet)
+  library(dplyr)
+  library(nycgeo)
+  library(dplyr)
+  
+  
+  options(scipen = 999)
+  
+  data <- read.csv("./pollen.csv", header = TRUE)
+  pollen_data <- data[,c("Borough","Influx_trees","Influx_trees_alrg","Influx_plat","Influx_que","Influx_amb",
+                                "Influx_ulm","Influx_pop","Influx_carya","Influx_poa","Influx_acer","Influx_fagus",
+                                "Influx_bet","Influx_frax","Latitude","Longitude")]
+  pollen_data <- st_as_sf(pollen_data, coords = c("Longitude", "Latitude"),  # specify coordinate columns
+                          crs = 4326) # assign a coordinate reference system (WGS84)
+  
+  pollen_map = nyc_boundaries(geography = "tract") #NYC MAP
+  
   #borough-specific setup
   borough_data <- pollen_data[pollen_data$Borough == borough, ]
   borough_data <- st_transform(borough_data, 4326)
@@ -58,40 +57,23 @@ borough_map <- function(borough) {
   map1 <- tm_shape(pollen_map_borough) + #sets shape to borough map
     tm_polygons(fill = "mean_influx", #maps based on pollen influx
                 title = "Avg Tree Pollen Influx",
-                palette = "brewer.greens", #color palette
+                palette = "YlOrRd", #color palette
                 border.alpha = 0.1) + #border thickness
-    tm_title(title)
+    tm_layout(title = title)
   
 
   map2 <- tm_shape(pollen_buffers) +
     tm_polygons(fill = "mean_influx",
                 title = "Avg Tree Pollen Influx",
-                palette = "brewer.greens",
+                palette = "YlOrRd",
                 col_alpha = 0.5) +
     tm_shape(borough_data) +  # Add collection points
     tm_dots(size = 0.05, col = "black", alpha = 0.8) +
     tm_title(paste(title,"(250m buffer)")) + 
     tm_legend(item.height = -10, item.width = -10)
- 
-   map3 <- tm_shape(pollen_map_borough) +
-    tm_polygons(fill = "mean_influx",       # color tracts by mean influx
-                palette = "brewer.greens",
-                border.alpha = 0.3,
-                id = "geoid") +             # show tract ID on hover
-    tm_shape(pollen_buffers) +
-    tm_borders(col = "blue", lwd = 2, alpha = 0.6) +  # outline buffers
-    tm_shape(borough_data) +
-    tm_dots(size = 0.05, col = "black", alpha = 0.8) +
-    tm_layout(title = paste(title, "(Tracts + 250m Buffers)"))
   
-  #print(map1)
-  #print(map2)
-  print(map3)
-  
+  print(map1)
+  print(map2)
 }
 
 borough_map("Manhattan")
-borough_map("Bronx")
-borough_map("Queens")
-borough_map("Staten Island")
-borough_map("Brooklyn")
